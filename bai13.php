e4d4ed4e45223e76d261eb2fce8fdc82ec5dc9a9
unknown <nthuy311023@gmail.com>
2024-10-08 00:34:18 +0700

    huy update 1

--- bai13.php
+++ bai13.php
@@ -1,67 +1,96 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="vi">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>thứ trong tuần</title>
+    <title>Thứ trong tuần</title>
 
     <style>
-        body{
+        body {
             display: flex;
             justify-content: center;
             align-items: center;
             height: 50vh;
+            font-family: Arial, sans-serif;
         }
-        .contain{
-           
+        .contain {
             border: 1px solid black;
-            height: auto;
             width: 500px;
             background-color: #ffcccc;
-            padding: 0px 10px 30px 10px;
+            padding: 20px;
+            border-radius: 10px;
         }
-        .head{
+        .head {
             text-align: center;
-           background-color: #fb88c2;
+            background-color: #fb88c2;
+            padding: 10px 0;
+            font-size: 1.5rem;
+            font-weight: bold;
         }
-        .giua{
+        .giua {
             display: flex;
-            justify-content: center;          
+            justify-content: center;
+            margin-top: 20px;
         }
-        .text{
+        .text {
             width: 50px;
-            margin: 0px 10px 0px 10px;
+            margin: 0px 10px;
         }
-        .bottom{
-            margin-top: 10px;
+        .bottom {
+            margin-top: 20px;
             display: flex;
-            justify-content: center; 
+            justify-content: center;
         }
-
+        .result {
+            text-align: center;
+            font-size: 1.2rem;
+            color: #333;
+        }
     </style>
 </head>
 <body>
-    <form action=""method="post" class="contain" >
+<?php
+    $result = "";
+    if (isset($_POST['submit'])) {
+        $day = $_POST['ngay'];
+        $month = $_POST['thang'];
+        $year = $_POST['nam'];
 
-     
-            <div class ="head">
-                <h1>Tìm Thứ Trong Tuần</h1>
-            </div>
-            <div class="giua">
-                Ngày/Tháng/Năm: 
-                <input type="text" class="text" name="ngay" min="1" max="31" required>/
-                <input type="text" class="text"name="thang" min="1" max="12" required>/
-                <input type="text" class="text" name="nam" min="1900"required >
-                <input type="submit" value="Tìm thứ trong tuần">
-                   
-            </div>
-            <div class="bottom">
-            <input type="text" name="hienthi" readonly style="width: 60%;">
+        // Tạo chuỗi ngày hợp lệ
+        $date = "$year-$month-$day";
 
-            </div>
-       
+        // Lấy thứ trong tuần
+        $weekday = date('l', strtotime($date));
 
+        // Chuyển thứ sang tiếng Việt
+        $days = array(
+            'Monday' => 'Thứ Hai',
+            'Tuesday' => 'Thứ Ba',
+            'Wednesday' => 'Thứ Tư',
+            'Thursday' => 'Thứ Năm',
+            'Friday' => 'Thứ Sáu',
+            'Saturday' => 'Thứ Bảy',
+            'Sunday' => 'Chủ Nhật'
+        );
+        
+        $weekday_in_vietnamese = $days[$weekday];
+        $result = "Ngày $day tháng $month năm $year là $weekday_in_vietnamese";
+    }
+?>
+    <form action="" method="post" class="contain">
+        <div class="head">
+            Tìm Thứ Trong Tuần
+        </div>
+        <div class="giua">
+            Ngày/Tháng/Năm: 
+            <input type="text" class="text" name="ngay" min="1" max="31" placeholder="Ngày" required> /
+            <input type="text" class="text" name="thang" min="1" max="12" placeholder="Tháng" required> /
+            <input type="text" class="text" name="nam" min="1900" placeholder="Năm" required>
+            <input type="submit" name="submit" value="Tìm thứ trong tuần">
+        </div>
+        <div class="bottom">
+            <input type="text" name="hienthi" readonly style="width: 100%; text-align: center;" value="<?php echo $result; ?>">
+        </div>
     </form>
-    
 </body>
 </html>
